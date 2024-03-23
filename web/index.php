29af1176be89d2dbf3ca83672ad06d0113f0eec4
Milinserv <107671454+Milinserv@users.noreply.github.com>
2024-03-24 00:32:30 +0400

    changed index file, deleted unused files

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
+use yii\web\Application;
 
-// comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
@@ -9,4 +9,4 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
 
-(new yii\web\Application($config))->run();
+(new Application($config))->run();
